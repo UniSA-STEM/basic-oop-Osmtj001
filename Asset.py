ef764f72abf7b75f7d60221bd9abd7415f9e4c55
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-10-15 17:09:54 +1030

    Create initial class which stores asset data - encryption by default is false

--- Asset.py
+++ Asset.py
@@ -6,3 +6,13 @@ ID: <110316757>
 Username: <Osmtj001>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+
+class Asset:
+
+"Will represent assets within the Main Module. These assets can become encrypted, and used by hackers within the main Module or stores in the Rigs"
+
+    def __init__(self, name:str, description:str):
+        self.name = name
+        self.description = description
+        self.encrypted = False
+
