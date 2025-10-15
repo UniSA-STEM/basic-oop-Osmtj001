610d4961f28af3cacc958fba3de0a4954d892ece
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-10-15 17:15:37 +1030

    Create decrypt def which allows input to decrypt items

--- Asset.py
+++ Asset.py
@@ -9,10 +9,27 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 
 class Asset:
 
-"Will represent assets within the Main Module. These assets can become encrypted, and used by hackers within the main Module or stores in the Rigs"
+
+#Will represent assets within the Main Module. These assets can become encrypted, and used by hackers within the main Module or stores in the Rigs
 
     def __init__(self, name:str, description:str):
         self.name = name
         self.description = description
         self.encrypted = False
 
+
+    def encrypt(self):
+
+        if self.encrypted == False:
+            self.encrypted = True
+            print(f'{self.name} Has been encrypted within the Main Module.')
+
+        else:
+            print(f'{self.name} Has been already been encrypted within the Main Module.')
+
+
+    def decrypt(self):
+        if self.encrypted == True:
+            print(f'{self.name} Has been decrypted within the Main Module.')
+        else:
+            print(f'{self.name} Has been already been decrypted within the Main Module.')
