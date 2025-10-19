8b458c2b50cb4bac73537adf1ed0397d3af1718c
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-10-19 22:13:55 +1030

    Finalized main function, with clean syntax and helper functions

--- Main.py
+++ Main.py
@@ -41,11 +41,15 @@ def testAttack():
     h.rig.storage.append(Asset('Data Spike', 'Used in battles.'))
     target = Rig('Lex Luther')
     print('Before Attack Launched')
+    print('-' * 30)
     print(h)
+    print('=' * 30)
     print(target)
     h.attack(target)
     print('After Attack')
+    print('-' * 30)
     print(h)
+    print('=' * 30)
     print(target)
 
 def testAttackExtraction():
@@ -61,12 +65,83 @@ def testAttackExtraction():
     a.encrypt = False
     target.storage.append(a)
     print('Before Attack Launched')
+    print('-' * 30)
     print(h)
+    print('=' * 30)
     print(target)
+    print('=' * 30)
     h.attack(target)
     h.attack(target)
     print('After Attack')
+    print('-' * 30)
     print(h)
+    print('=' * 30)
+    print(target)
+
+def testEncryptedExtraction():
+    h = Hacker('Superman')
+    h.rig = Rig('SuperRig')
+    h.rig.storage.append(Asset('Data Spike', 'Used in battles.'))
+    h.rig.storage.append(Asset('Data Spike', 'Used in battles.'))
+    h.rig.storage.append(Asset("Removable Drive", 'Found in rigs and used for extraction.'))
+    target = Rig('Lex Luther')
+    target.broken = True
+    e = Asset('Kryptonite', 'Supermans biggest weakness')
+    e.encrypted = True
+    target.storage.append(e)
+    print('Before Attack Launched')
+    print('-' * 30)
+    print(h)
+    print('=' * 30)
+    print(target)
+    print('=' * 30)
+    h.attack(target)
+    print('After Attack')
+    print('-' * 30)
+    print(h)
+    print('=' * 30)
+    print(target)
+
+def testExposedAttack():
+    h = Hacker('Superman')
+    h.rig = Rig('SuperRig')
+    h.rig.storage.append(Asset('Data Spike', 'Used in battles.'))
+    h.traceLevel = h.traceThreshold
+    target = Rig('Lex Luther')
+    print('Before attack Launched')
+    print('-' * 30)
+    print(h)
+    print('=' * 30)
+    h.attack(target)
+
+def testUpgrade():
+    h = Hacker('Superman')
+    h.rig = Rig('SuperRig')
+    h.inventory.append(Asset('Hardware Patch', 'Used to upgrade rigs.'))
+    print('Before Upgrade')
+    print('-' * 30)
+    print(h.rig.name, 'is currently Level', h.rig.upgradeLevel)
+    print('=' * 30)
+    h.upgradeRig()
+    print('After Upgrade')
+    print('-' * 30)
+    print(h.rig.name, 'is now Level', h.rig.upgradeLevel)
+
+def testRepair():
+    h = Hacker('Superman')
+    h.rig = Rig('SuperRig')
+    h.rig.damageCounter = h.rig.upgradeLevel + 2
+    h.rig.broken = True
+    print('Rig Before Repair')
+    print('-' * 30)
+    h.inventory.append(Asset('CryptoToken', 'Used to acquire or repair rigs.'))
+    print(f'{h.rig.name}:', 'Broken?', {h.rig.broken}, 'Damage:', {h.rig.damageCounter})
+    print('=' * 30)
+    h.repairRig()
+    print('Rig After Repair')
+    print('=' * 30)
+    print(f'{h.rig.name}:', 'Broken?', {h.rig.broken}, 'Damage:', {h.rig.damageCounter})
+
 
 
 #Calls the above functions with the formatting from Normalizer to return a clean output of tests run
@@ -75,3 +150,7 @@ if __name__ == '__main__':
     normalizerformat(testNoDataspike, 'Test without a dataspike')
     normalizerformat(testAttack, 'Simulation of full attack')
     normalizerformat(testAttackExtraction, 'Simulation with extraction')
+    normalizerformat(testEncryptedExtraction, 'Simulation with encrypted extraction')
+    normalizerformat(testExposedAttack, 'Simulation of attack while exposed')
+    normalizerformat(testUpgrade, 'Simulation of rig upgrade')
+    normalizerformat(testRepair, 'Simulation of repair rig')
