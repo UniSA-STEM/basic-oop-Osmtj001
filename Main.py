5342b8495b0600dbb144925e243054541ff437c4
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-10-19 16:28:36 +1030

    Added more testing functionality

--- Main.py
+++ Main.py
@@ -12,26 +12,41 @@ from Rig import Rig
 from Asset import Asset
 import random
 
+#Initialising Hacker & Rig for simulation. The hacker & rig names are not case sensitive
+print('***Initial Assignment of Hacker Name & Rig Name***')
 Jane = Hacker('Jane')
 Steve = Hacker('Steve')
-
 rig1 = Rig('Orion')
 rig2 = Rig('Didox')
 
+#Assigns the Rig to a Hacker. In this case Rig1 has been assigned to Jane
+print('***Rig Acquisition by the Hacker***')
 Jane.rigAcquisition(rig1)
 Steve.rigAcquisition(rig2)
 
+#Generating assets for Rig storage. All items which cannot go into rig storage will be allocated to the Hackers inventory
+print('***Rig 1 & 2 have generated assets***')
+rig1.generateAsset(Jane)
+rig1.generateAsset(Jane)
+rig1.generateAsset(Jane)
 rig1.generateAsset(Jane)
 rig2.generateAsset(Steve)
 rig2.generateAsset(Steve)
 rig2.generateAsset(Steve)
 
-Jane.storeInRig()
+#This will find a specific item & store it within the hacker inventory. If no asset is given, all items will be transferred
+print('***Functionality for storing items in Rig***')
+Jane.storeInRig('Security Chip')
 Steve.storeInRig()
 
+#Takes a Crypto Token (if stores with inventory) & Encrypts with the use of 1x Security Chip (if available)
+print('***Encryption of item***')
 Steve.encryption('CryptoToken')
 
+print('***Upgrading of Rig***')
 Steve.upgradeRig()
-Jane.rigRetrival()
 
-print(Steve)
+Steve.attack(rig1)
+
+print('***Retrival of all asstets from Rig***')
+Steve.rigRetrival('Security Chip')
