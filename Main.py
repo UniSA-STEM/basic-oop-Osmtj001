3f2227716ae17e4c9beb7a6570a418490dbef5f4
Osmtj001 <osmtj001@mymail.unisa.edu.au>
2025-10-19 15:19:19 +1030

    Inputting basic testing functionality

--- Main.py
+++ Main.py
@@ -6,4 +6,32 @@ ID: <110316757>
 Username: <Osmtj001>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+#Initialises all required files
+from Hacker import Hacker
+from Rig import Rig
+from Asset import Asset
+import random
 
+Jane = Hacker('Jane')
+Steve = Hacker('Steve')
+
+rig1 = Rig('Orion')
+rig2 = Rig('Didox')
+
+Jane.rigAcquisition(rig1)
+Steve.rigAcquisition(rig2)
+
+rig1.generateAsset(Jane)
+rig2.generateAsset(Steve)
+rig2.generateAsset(Steve)
+rig2.generateAsset(Steve)
+
+Jane.storeInRig()
+Steve.storeInRig()
+
+Steve.encryption('CryptoToken')
+
+Steve.upgradeRig()
+Jane.rigRetrival()
+
+print(Steve)
